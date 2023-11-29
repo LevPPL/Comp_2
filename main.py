131d58108aad8752fea6cd5f9174d6f212b08864
LevPPL <levacuber@gmail.com>
2023-11-29 10:59:05 -0500

    Classwork 28/11 ding beng

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-def def_count_emoji(message):
-    if message.count(":-)")>message.count(":-("):
-        print("happy")
-    elif message.count(":-(")>message.count(":-)"):
-        print("sad")
-    elif message.count(":-(")==message.count(":-)"):
-        print("unsure")
-    else
-        print("none")
+# def def_count_emoji(message):
+#     if message.count(":-)")>message.count(":-("):
+#         print("happy")
+#     elif message.count(":-(")>message.count(":-)"):
+#         print("sad")
+#     elif message.count(":-(")==message.count(":-)"):
+#         print("unsure")
+#     else
+#         print("none")
 
 
 
